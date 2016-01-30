64142f4044b469510f2fe20dc3326a02ea91d891
harshul1610 <harshulrobo@gmail.com>
2016-01-30 10:25:10 +0530

    added security

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,7 @@ from django.contrib import auth
 from django.db import models
 from django.utils import timezone
 
+
 class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
